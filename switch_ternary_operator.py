7aacc213641908f2888e87af5dd0e728bf34f8bc
jaymarc21 <>
2025-03-08 03:43:45 +0800

    Last Sync: 2025-03-07 19:43:45 (Mobile)

--- switch_ternary_operator.py
+++ switch_ternary_operator.py
@@ -1,4 +1,3 @@
-
 day_messages = {
     "monday": "Today is Monday.",
     "tuesday": "Today is Tuesday.",
@@ -8,15 +7,8 @@ day_messages = {
     "saturday": "Today is Saturday.",
     "sunday": "Today is Sunday."
 }
-
 day = input("Enter a day of the week: ").strip().lower()
-
-message = day_messages.get(day)
-
-if message:
-    day_type = "Weekend" if day in ("saturday", "sunday") else "Weekday"
-    print(message)
-    print("It's a", day_type + "!")
-else:
-    print("Invalid day entered.")
-\
+message = day_messages.get(day, "Invalid day entered.")
+day_type = "Weekend" if day in ("saturday", "sunday") else "Weekday"
+print(message)
+print("It's a", day_type + "!")
